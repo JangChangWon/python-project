015276098de2d4c6a62918d77f515cee87dfade0
ChangWonKang <61802520+JangChangWon@users.noreply.github.com>
2020-06-03 15:32:31 +0900

    1
    This file is available to rasberrypi for picam
    so please use cmd and just clone and write it

---
+++ access_picam.py
@@ -0,0 +1,31 @@
+# USAGE
+# python access_camera.py
+
+# import the necessary packages
+from imutils.video import VideoStream
+import imutils
+import time
+import cv2
+
+print("[INFO] starting video stream...")
+vs = VideoStream(usePiCamera=True, resolution=(640, 480)).start()
+time.sleep(2.0)
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the  video stream and resize it to have a
+	# maximum width of 400 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("v"):
+		break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
